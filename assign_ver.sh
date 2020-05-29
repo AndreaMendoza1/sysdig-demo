ea856d492f561daddf48ed5684208151084f1c93
root <root@nbparking-lb1-e8979d80a94bc16b.elb.us-east-1.amazonaws.com>
2020-05-29 16:38:32 -0500

    last change 4

--- assign_ver.sh
+++ assign_ver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sed s/{{BUILD_NUMBER}}/$1/g deployment.yaml > temp.yaml
-oc project default
-kubectl apply -f temp.yaml
+sed s/{{BUILD_NUMBER}}/$1/g deployment.yaml | kubectl -f -
+/usr/local/bin/oc project default
+cat 
 kubectl apply -f service.yaml
 rm -f temp.yaml
