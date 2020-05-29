0cf6c77a96394e654633c574e8aff15c968d6f22
root <root@nbparking-lb1-e8979d80a94bc16b.elb.us-east-1.amazonaws.com>
2020-05-29 16:46:51 -0500

    last 3

--- assign_ver.sh
+++ assign_ver.sh
@@ -2,8 +2,8 @@
 export KUBECONFIG=kubeconfig
 export TOKEN=zu7pIRBLhy5P923iMZDKQM4Zxrr_MFCvEiZeS1MHIs8
 oc login --token=$TOKEN --server=https://c100-e.eu-de.containers.cloud.ibm.com:31569
-/usr/local/bin/oc project default
+oc project default
 touch temp.yaml
 sed s/{{BUILD_NUMBER}}/$1/g deployment.yaml > temp.yaml
-kubectl -f temp.yaml
+kubectl apply -f temp.yaml
 rm -f temp.yaml
