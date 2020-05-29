0d2e110bcf6ef2c375eef9a67fa29cb130158b5a
root <root@nbparking-lb1-e8979d80a94bc16b.elb.us-east-1.amazonaws.com>
2020-05-29 13:02:59 -0500

    demo01

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ app.get('/getpodinfo', function (req, res) {
 var server = app.listen(process.env.MY_PORT || 4444, function () {
     var port = server.address().port;
     
-    console.log("Server started! Listen Port = %s", port)
+    console.log("Server started! Listen Port = %s w changes", port)
 })
